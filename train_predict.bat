30b5658814aad2e3ca076b886a0b4ad04f29f524
laixch <754578682@qq.com>
2018-10-29 09:38:22 +0800

    delete bat file

--- train_predict.bat
+++
@@ -1,7 +0,0 @@
-cd /d %~dp0
-pause(0)
-python ./src/align_dataset_mtcnn.py ./dataset_without_align ./dataset_without_align_160 --image_size 160 --margin 0 --random_order
-pause(0)
-python ./src/classifier.py TRAIN ./dataset_without_align_160 E:\AI_DATA\facenet_model%please replace with your own directory containing the meta_file and ckpt_file% ./classifier.pkl
-pause(0)
-python ./src/camera_predict.py E:\AI_DATA\facenet_model%replace with your own% ./classifier.pkl
